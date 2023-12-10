c3993a3280e929115f870b3833f3e7428f6b807f
matsu82engin <toshiayo59@gmail.com>
2023-12-10 07:52:28 +0000

    最初のテスト

--- nuxt-app/artstyle-navigator-front/test/static-pages.spec.js
+++ nuxt-app/artstyle-navigator-front/test/static-pages.spec.js
@@ -1,7 +1,19 @@
 import { mount } from '@vue/test-utils';
-import App from "@/components/app.vue";
+import Home from '@/pages/index.vue';
+import Help from '@/pages/help.vue';
+import About from '@/pages/about.vue';
 
-test("test App Component",function(){
-  const wrapper = mount(App);
-  expect(wrapper.text()).toBe('Hello World')
+test("test static-pages-view-home response",function(){
+  const wrapper = mount(Home);
+  expect(wrapper.text()).toContain('This is the home page')
+})
+
+test("test static-pages-view-help response", () => {
+  const wrapper = mount(Help);
+  expect(wrapper.text()).toContain('This is the help page')
+})
+
+test("test static-pages-view-about response", () => {
+  const wrapper = mount(About);
+  expect(wrapper.text()).toContain('This is the about page')
 })
