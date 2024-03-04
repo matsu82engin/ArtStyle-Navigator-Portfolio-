6e3d93a721968931d651b18dd747ff83f818f7e2
matsu82engin <toshiayo59@gmail.com>
2024-03-04 06:58:51 +0000

    ユーザー登録のエラー修正

--- nuxt-app/artstyle-navigator-front/pages/account/signup.vue
+++ nuxt-app/artstyle-navigator-front/pages/account/signup.vue
@@ -66,14 +66,27 @@ export default {
         };
     },
     methods: {
-        userRegister() {
-            this.$axios.post('/api/v1/auth', this.user).then(() => {
-            this.$router.push(`/users/${this.$auth.user.id}`);
-            })
-              .catch((error) => {
-                ErrorMessage.methods.setErrorMessage.call(this, error);
-            });
-        },
+      userRegister() {
+      // thenメソッドを使ってレスポンスを処理する
+        this.$axios.post('/api/v1/auth', this.user)
+          .then(response => {
+        // ログイン処理
+        this.$auth.loginWith('local', {
+          data: {
+            email: this.user.email,
+            password: this.user.password
+          }
+        }).then(() => {
+          // ログイン後の処理
+          this.$router.push(`/users/${response.data.data.id}`);
+        }).catch(error => {
+          ErrorMessage.methods.setErrorMessage.call(this, error);
+        });
+      })
+      .catch(error => {
+        ErrorMessage.methods.setErrorMessage.call(this, error);
+      });
+      },
     },
     components: { ErrorMessage }
 }
