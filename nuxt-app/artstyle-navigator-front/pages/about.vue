976209b7cace410a089849a23980003e7b6cb0d4
matsu82engin <toshiayo59@gmail.com>
2024-05-07 06:59:20 +0000

    persestedstate,clientinit module 追加

--- nuxt-app/artstyle-navigator-front/pages/about.vue
+++ nuxt-app/artstyle-navigator-front/pages/about.vue
@@ -4,10 +4,6 @@
     <h2>ログイン状態: {{ this.$auth.loggedIn }}</h2>
     <h2>ログインユーザー情報: {{ this.$auth.user }}</h2>
     <h2>state情報: {{ this.$store.state.auth.user }}</h2>
-    <h2>その他ユーザー情報: 
-      {{ this.$auth.user }}
-      {{ this.$auth.loggedIn }}
-      {{ this.$store.state.auth.loggedIn }}    
-    </h2>
+    <p v-if="this.$auth.loggedIn">トークン: {{this.$auth.getToken('local')}}</p>
   </div>
 </template>
---
+++ nuxt-app/artstyle-navigator-front/plugins/sessionStorage.js
@@ -0,0 +1,11 @@
+// import createPersistedState from 'vuex-persistedstate';
+
+// export default ({ store }) => {
+//   window.onNuxtReady(() => {
+//     createPersistedState({
+//       key: 'persisted-key',
+//       // paths: [''],
+//       storage: window.sessionStorage
+//     })(store)
+//   });
+// }
