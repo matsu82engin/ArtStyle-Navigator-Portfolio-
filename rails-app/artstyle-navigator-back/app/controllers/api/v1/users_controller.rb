d2cf128c92006a32e26d662df26e6076e8b9d1f5
matsu82engin <toshiayo59@gmail.com>
2024-05-11 09:30:41 +0000

    users index アクション追加

--- rails-app/artstyle-navigator-back/app/controllers/api/v1/users_controller.rb
+++ rails-app/artstyle-navigator-back/app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_api_v1_auth_user!
+  before_action :authenticate_api_v1_auth_user!, except: [:index]
 
   # GET /api/v1/users/:id
   def show
     render json: current_api_v1_auth_user
+  end
+
+  # GET /api/v1/users
+  def index
+    users = User.all
+    render json: users.as_json(only: [:id, :name, :email, :created_at])
   end
 end
--- rails-app/artstyle-navigator-back/db/seeds.rb
+++ rails-app/artstyle-navigator-back/db/seeds.rb
@@ -1,14 +1,14 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
-             email: "sample@example.com",
-             password:              "password")
+User.create!(name: 'Example User',
+             email: 'sample@example.com',
+             password: 'password')
 
 # 追加のユーザーをまとめて生成する
 9.times do |n|
-  name  = "user#{n + 1}"
-  email = "#{ n + 1}@example.com"
-  password = "password"
-  User.create!(name:  name,
+  name = "user#{n + 1}"
+  email = "#{n + 1}@example.com"
+  password = 'password'
+  User.create!(name: name,
                email: email,
-               password:              password)
+               password: password)
 end
