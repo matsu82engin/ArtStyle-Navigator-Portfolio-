88469a0d77d9dd10e27d8660fcf58783b9154c45
matsu82engin <toshiayo59@gmail.com>
2024-05-14 08:13:23 +0000

    Add Home Components

--- nuxt-app/artstyle-navigator-front/pages/index.vue
+++ nuxt-app/artstyle-navigator-front/pages/index.vue
@@ -1,10 +1,50 @@
 <template>
-  <div>
-    <h1>This is the home page</h1>
-  </div>
+  <v-app>
+    <v-sheet>
+      <v-container
+        fluid
+        :style="{ maxWidth: '1280px' }"
+      >
+        <v-row
+          v-for="(menu, i) in menus"
+          :key="`menu-${i}`"
+        >
+          <v-col 
+            cols="12"
+          >
+            <div
+              :is="`home-${menu.title}`" 
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+    <app-footer />
+  </v-app>
 </template>
+
 <script>
- export default {
-  
- }
+import HomeAbout from '~/components/Home/HomeAbout'
+import HomeAnotherArtStyle from '~/components/Home/HomeAnotherArtStyle'
+import HomeFindMyArtStyleButton from '~/components/Home/HomeFindMyArtStyleButton'
+import HomeManyArtStyle from '~/components/Home/HomeManyArtStyle'
+
+export default {
+  components: {
+    HomeAbout,
+    HomeAnotherArtStyle,
+    HomeFindMyArtStyleButton,
+    HomeManyArtStyle,
+  },
+  data () {
+    return {
+      menus: [
+        { title: 'about'},
+        { title: 'another-art-style'},
+        { title: 'find-my-art-style-button'},
+        { title: 'many-art-style'}
+      ]
+    }
+  }
+}
 </script>
