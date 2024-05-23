4db7eaa47b5ef573fe8db3fa94914043266cc3c1
matsu82engin <toshiayo59@gmail.com>
2024-05-23 02:19:22 +0000

    Add Vuetify Custom CSS

--- nuxt-app/artstyle-navigator-front/pages/users/index.vue
+++ nuxt-app/artstyle-navigator-front/pages/users/index.vue
@@ -28,11 +28,42 @@
     <div v-else>
       ユーザーが取得できませんでした
     </div>
+
+
+    <v-card-title>
+        VuetifyカスタムCSSの検証
+    </v-card-title>
+    <v-card-text>
+        ipad（768px）とmobile（426px）で表示・非表示
+    </v-card-text>
+    
+    <v-card-text>
+      <v-card
+        v-for="(cls, i) in customClass"
+        :key="`cls-${i}`"
+        :color="cls.color"
+        :class="cls.name"
+      >
+        <v-card-text>
+          {{ cls.des }}
+        </v-card-text>
+      </v-card>
+    </v-card-text>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      customClass: [
+          { name: 'hidden-ipad-and-down', color: 'error', des: 'ipad未満で隠す' },
+          { name: 'hidden-ipad-and-up', color: 'info', des: 'ipad以上で隠す' },
+          { name: 'hidden-mobile-and-down', color: 'success', des: 'mobile未満で隠す' },
+          { name: 'hidden-mobile-and-up', color: 'warning', des: 'mobile以上で隠す' }
+        ]
+    }
+  },
   async asyncData ({ $axios }) {
     let users = []
     await $axios.$get('/api/v1/users')
