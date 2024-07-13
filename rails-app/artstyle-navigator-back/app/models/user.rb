9c13c7417d4330ef863d98af490496020b79c9c7
matsu82engin <toshiayo59@gmail.com>
2024-07-13 07:05:04 +0000

    add refresh_token

--- rails-app/artstyle-navigator-back/app/models/user.rb
+++ rails-app/artstyle-navigator-back/app/models/user.rb
@@ -11,4 +11,14 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   # email の presence: true は devise ですでに設定されているため書かない
   validates :email, length: { maximum: 255 }
+
+  # リフレッシュトークンのJWT IDを保存
+  def remember(jti)
+    update!(refresh_jti: jti)
+  end
+  
+  # リフレッシュトークンのJWT IDを削除
+  def forget
+    update!(refresh_jti: nil)
+  end
 end
