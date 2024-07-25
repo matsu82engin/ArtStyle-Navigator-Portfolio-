add140b86ca9226f6e1f5f8b45f6394436b3714e
matsu82engin <toshiayo59@gmail.com>
2024-07-25 06:43:42 +0000

    Add sessionize service module

--- rails-app/artstyle-navigator-back/app/controllers/application_controller.rb
+++ rails-app/artstyle-navigator-back/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@
 
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionsController::Cookies
   skip_before_action :verify_authenticity_token, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
