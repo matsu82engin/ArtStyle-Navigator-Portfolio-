95aac502b173c84b5680613c22ec5f13189e1c4c
matsu82engin <toshiayo59@gmail.com>
2024-12-17 08:01:54 +0000

    Add User model column art_style art_supply

--- rails-app/artstyle-navigator-back/db/schema.rb
+++ rails-app/artstyle-navigator-back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_13_062753) do
+ActiveRecord::Schema.define(version: 2024_12_17_071455) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2024_07_13_062753) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "refresh_jti"
+    t.string "art_style"
+    t.string "art_supply"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
