233f9b06f8ff4b368813b93e9c315033b43998b4
matsu82engin <toshiayo59@gmail.com>
2025-08-30 06:29:24 +0000

    fix: users_controller

--- rails-app/artstyle-navigator-back/app/controllers/api/v1/users_controller.rb
+++ rails-app/artstyle-navigator-back/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_api_v1_user!, except: [:index]
+  before_action :set_user, only: [:show]
 
   # GET /api/v1/users
   def index
@@ -9,6 +10,15 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /api/v1/users/:id
   def show
-    render json: current_api_v1_user
+    # render json: current_api_v1_user
+    # 必要なユーザーデータのみレスポンスする
+    render json: @user.as_json(only: [:id, :name, :email, :created_at])
+  end
+
+  private
+
+  def set_user
+    # params[:id] でユーザーを探す。404 発生時は => application_contoroller の rescue_from
+    @user = User.find(params[:id])
   end
 end
